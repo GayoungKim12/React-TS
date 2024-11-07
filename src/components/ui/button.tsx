04c6d1dee42e1c776d2281e6103467230f925059
GayoungKim12 <cloud9b58@gmail.com>
2024-11-07 16:37:36 +0900

    feat: icon button size sm/lg로 분리

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
@@ -21,6 +20,8 @@ const buttonVariants = cva(
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        iconSm: "h-10 w-10",
+        iconLg: "h-14 w-14",
       },
     },
     defaultVariants: {
