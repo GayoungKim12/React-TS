2a712e9e3cefed3ad0de997600d604c91f239d55
GayoungKim12 <cloud9b58@gmail.com>
2024-11-08 14:11:41 +0900

    refactor: shared 내부 types 폴더 위치 변경

--- src/shared/ui/types/components/typography.types.ts
+++
@@ -1,7 +0,0 @@
-import { Color, Size } from "../common/theme.types";
-
-export type TypographyElement = "h1" | "h2" | "h3" | "h4" | "p" | "ul" | "ol" | "a" | "blockquote" | "code";
-export type TextSize = "inherit" | Size;
-export type Weight = "light" | "normal" | "medium" | "semibold" | "bold" | "extrabold";
-export type TextAlign = "left" | "center" | "right";
-export type TextColor = Color;
