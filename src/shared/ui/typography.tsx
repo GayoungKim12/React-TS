fd568e43fa16a12639f0130f72eebb29f103c51e
GayoungKim12 <cloud9b58@gmail.com>
2024-11-08 14:05:52 +0900

    refactor: Typography UI Component Type 분리

--- src/shared/ui/typography.tsx
+++ src/shared/ui/typography.tsx
@@ -2,22 +2,24 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
+import { TextAlign, TextColor, TextSize, TypographyElement, Weight } from "./types/components/typography.types";
 
 const typographyVariants = cva("", {
   variants: {
-    variant: {
-      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
-      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+    component: {
+      h1: "scroll-m-20 tracking-tight",
+      h2: "scroll-m-20 border-b pb-2 tracking-tight first:mt-0",
+      h3: "scroll-m-20 tracking-tight",
+      h4: "scroll-m-20 tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",
       ul: "my-6 ml-6 list-disc [&>li]:mt-2",
       ol: "my-6 ml-6 list-decimal [&>li]:mt-2",
-      a: "font-medium underline underline-offset-4",
+      a: "underline underline-offset-4",
       blockquote: "mt-6 border-l-2 pl-6 italic",
-      code: "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+      code: "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono",
     },
     size: {
+      inherit: "text-inherit",
       xs: "text-xs",
       sm: "text-sm",
       base: "text-base",
@@ -26,6 +28,11 @@ const typographyVariants = cva("", {
       "2xl": "text-2xl",
       "3xl": "text-3xl",
       "4xl": "text-4xl",
+      "5xl": "text-5xl",
+      "6xl": "text-6xl",
+      "7xl": "text-7xl",
+      "8xl": "text-8xl",
+      "9xl": "text-9xl",
     },
     weight: {
       light: "font-light",
@@ -49,7 +56,7 @@ const typographyVariants = cva("", {
     },
   },
   defaultVariants: {
-    variant: "p",
+    component: "p",
     size: "base",
     weight: "normal",
     align: "left",
@@ -57,29 +64,23 @@ const typographyVariants = cva("", {
   },
 });
 
-type TypographyElement = "h1" | "h2" | "h3" | "h4" | "p" | "ul" | "ol" | "a" | "blockquote" | "code" | "table" | "tr" | "th" | "td";
-type Size = "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
-type Weight = "light" | "normal" | "medium" | "semibold" | "bold" | "extrabold";
-type TextAlign = "left" | "center" | "right";
-type Color = "default" | "muted" | "primary" | "secondary" | "destructive";
-
 interface TypographyProps extends React.HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof typographyVariants> {
   asChild?: boolean;
-  as?: TypographyElement;
-  size?: Size;
+  component?: TypographyElement;
+  size?: TextSize;
   weight?: Weight;
   align?: TextAlign;
-  color?: Color;
+  color?: TextColor;
 }
 
-const Typography = React.forwardRef<HTMLParagraphElement, TypographyProps>(({ className, variant, size, weight, align, color, as, asChild = false, ...props }, ref) => {
+const Typography = React.forwardRef<HTMLParagraphElement, TypographyProps>(({ className, size, weight, align, color, component, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "p";
 
   return (
     <Comp
       className={cn(
         typographyVariants({
-          variant,
+          component,
           size,
           weight,
           align,
