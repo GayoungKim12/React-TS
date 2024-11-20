a60ae70ccd527ae7baf4d807433559d5525bee09
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-11-20 14:01:49 +0900

    layout: Change design

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,9 +4,9 @@ import { Typography } from "@/shared/components/ui/typography/typography";
 export default function MainPage() {
   return (
     <Flex>
-      <Flex className="mt-12" asChild>
+      <Flex className="mt-4 ml-8" asChild>
         <header>
-          <Typography component="h1" size="7xl" weight="bold" asChild>
+          <Typography component="h1" size="6xl" weight="bold" asChild>
             <h1>Practicing Shadcn/ui</h1>
           </Typography>
         </header>
