01ee495e8bde3fc4e7af15c60c33f648b27225f1
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-11-21 09:42:55 +0900

    feat: Set sidebar group

--- src/feature/sidebar/ui/Group.tsx
+++ src/feature/sidebar/ui/Group.tsx
@@ -1,16 +1,56 @@
-import Icon from "@/shared/components/icon";
-import { Flex } from "@/shared/components/ui/layout/flex";
-import { Typography } from "@/shared/components/ui/typography/typography";
+import { Icon, IconName } from "@/shared/components/icon";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/shared/components/ui/common/collapsible";
+import { SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarMenuSub, SidebarMenuSubItem } from "@/shared/components/ui/layout/sidebar";
+import { Link } from "react-router-dom";
+
+export type SidebarSubMenuItem = {
+  to: string;
+  label: string;
+};
+
+export type SidebarMenuItem = {
+  to: string;
+  label: string;
+  icon: IconName;
+  subItems?: SidebarSubMenuItem[];
+};
 
 interface GroupProps {
   subheading: string;
+  items: SidebarMenuItem[];
 }
 
-export default function Group({ subheading }: GroupProps) {
+export default function Group({ subheading, items }: GroupProps) {
   return (
-    <Flex>
-      <Typography>{subheading}</Typography>
-      <Icon name="AlarmClockIcon" size={16} strokeWidth={1.5} color="gray" />
-    </Flex>
+    <SidebarGroup>
+      <SidebarGroupLabel>{subheading}</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item: SidebarMenuItem) => (
+            <Collapsible key={item.to} defaultOpen className="group/collapsible">
+              <SidebarMenuItem key={item.to}>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    <Icon name={item.icon} strokeWidth={2} />
+                    {item.label}
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+              </SidebarMenuItem>
+              {item.subItems && (
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.subItems.map((subItem) => (
+                      <SidebarMenuSubItem key={subItem.to}>
+                        <Link to={`${item.to}/${subItem.to}`}>{subItem.label}</Link>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              )}
+            </Collapsible>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 }
