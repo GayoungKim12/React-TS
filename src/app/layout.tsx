6e215bc91c04f3f64bd1e6966ed7dc9faa647d50
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-11-22 16:43:35 +0900

    feat: Add header in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
-import { SidebarProvider, SidebarTrigger } from "@/shared/components/ui/layout/sidebar";
+import { Flex } from "@/shared/components/ui/layout/flex";
+import { SidebarProvider } from "@/shared/components/ui/layout/sidebar";
+import Header from "@/widgets/header/ui/Header";
 import AppSidebar from "@/widgets/sidebar/ui/AppSidebar";
 import { Outlet } from "react-router-dom";
 
@@ -6,10 +8,12 @@ export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger />
-        <Outlet />
-      </main>
+      <Flex direction="col" gap="xs" className="w-full" items="start" justify="start">
+        <Header />
+        <main className="w-full p-8">
+          <Outlet />
+        </main>
+      </Flex>
     </SidebarProvider>
   );
 }
